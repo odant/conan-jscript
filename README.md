deec85092bae86ca0d26f8b8010a267f316fdeee
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2019-03-27 19:50:38 +0300

    Add options for build with Ninja (default True), add build with MSBuild

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # conan-jscript
+jscript (based on Node.js) Conan package
+
+# Options
+
+## dll_sign
+Sign DLL (Windows only), default True
+
+## ninja
+Use Ninja build system, default True
+
+# Manual build package (in local folder)
+Set enviroment variable CONAN_USERNAME to "odant"
+In source package folder (where conanfile.py):
+
+    conan install . -if local -o jscript:dll_sign=False -o jscript:ninja=False -s arch=x86_64 -s build_type=Debug
+    conan source . -sf local
+    conan build . -bf local
+    conan package . -bf local -pf local/p
+
+Set package to *editable mode*:
+
+    conan editable add jscript/x.y.z.b@odant/testing local/p
+
+After change source (in folder *local*):
+
+    conan build . -bf local && conan package . -bf local -pf local/p
+    build your test project using that jscript/x.y.z.b@odant/testing
+
+Get patch (difference betwen source in package and local):
+
+    git diff --diff-filter=a --no-index -- src/ src/local
--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,8 @@
-# jscript Conan package manager
-# Dmitriy Vetutnev, Odant, 2018
+# jscript Conan package
+# Dmitriy Vetutnev, Odant, 2018-2019
 
 
-from conans import ConanFile, tools
+from conans import ConanFile, tools, MSBuild
 from conans.errors import ConanException
 import os, glob
 
@@ -27,10 +27,11 @@ class JScriptConan(ConanFile):
         "arch": ["x86_64", "x86", "mips"]
     }
     options = {
-        "dll_sign": [False, True]
+        "dll_sign": [False, True],
+        "ninja": [False, True]
     }
-    default_options = "dll_sign=True"
-    exports_sources = "src/*", "build.patch", "source.patch", "FindJScript.cmake"
+    default_options = "dll_sign=True", "ninja=True"
+    exports_sources = "src/*", "oda.patch", "FindJScript.cmake"
     no_copy_source = False
     build_policy = "missing"
     short_paths = True
@@ -48,24 +49,21 @@ class JScriptConan(ConanFile):
         # Only C++11
         if self.settings.compiler.get_safe("libcxx") == "libstdc++":
             raise Exception("This package is only compatible with libstdc++11")
-        # DLL sign, only Windows
+        # DLL sign, only on Windows
         if self.settings.os != "Windows":
             del self.options.dll_sign
 
     def requirements(self):
         self.requires("openssl/%s@%s/stable" % (self._openssl_version, self.user))
-        
+
     def build_requirements(self):
-        self.build_requires("ninja_installer/1.9.0@bincrafters/stable")
-        if self.settings.os == "Windows":
-            self.build_requires("nasm/2.13.01@conan/stable")
+        if self.options.ninja:
+            self.build_requires("ninja_installer/1.9.0@bincrafters/stable")
         if get_safe(self.options, "dll_sign"):
             self.build_requires("windows_signtool/[~=1.0]@%s/stable" % self.user)
 
     def source(self):
-        tools.patch(patch_file="build.patch")
-        return
-        tools.patch(patch_file="source.patch")
+        tools.patch(patch_file="oda.patch")
 
     def build(self):
         output_name = "jscript"
@@ -78,7 +76,6 @@ class JScriptConan(ConanFile):
                 output_name += "d"
         #
         flags = [
-            "--ninja",
             "--verbose",
             "--shared",
             "--dest-os=%s" % {
@@ -92,43 +89,50 @@ class JScriptConan(ConanFile):
                             }.get(str(self.settings.arch)),
             "--node_core_target_name=%s" % output_name
         ]
+        # !!! Without PCH build faild with MSBuild
+        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            flags.append("--with-pch")
+        # External OpenSSL
         openssl_includes = self.deps_cpp_info["openssl"].include_paths[0].replace("\\", "/")
-        openssl_lib_path = self.deps_cpp_info["openssl"].lib_paths[0].replace("\\", "/")
-        openssl_libs = []
-        for l in ["libcrypto", "libssl"]:
-            if self.settings.os == "Windows":
-                l = os.path.join(openssl_lib_path, l).replace("\\", "/") + ".lib"
-            else:
-                l = os.path.join(openssl_lib_path, l) + ".so.1.1"
-            openssl_libs.append(l)
+        openssl_libpath = self.deps_cpp_info["openssl"].lib_paths[0].replace("\\", "/")
         flags.extend([
             "--shared-openssl",
             "--shared-openssl-includes=%s" % openssl_includes,
+            "--shared-openssl-libpath=%s" % openssl_libpath
         ])
         if self.settings.os == "Windows":
-            flags.append("--shared-openssl-libname=%s" % ",".join(openssl_libs))
-        else:
-            flags.append("--shared-openssl-libpath=%s" % openssl_lib_path)
-        #
+            flags.append("--shared-openssl-libname=libcrypto.lib,libssl.lib")
+        # Build type, debug/release
         if self.settings.build_type == "Debug":
             flags.append("--debug")
             if self.settings.os == "Linux":
                 flags.append("--gdb")
-        # Run build
+        #
+        if self.options.ninja:
+            flags.append("--ninja")
+        #
         env = {}
         if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
             env = tools.vcvars_dict(self.settings, force=True)
             env["GYP_MSVS_VERSION"] = "2017"
             env["PLATFORM_TOOLSET"] = "v141"
-            # Use external ninja
-            ninjaDir = self.deps_cpp_info["ninja_installer"].bin_paths[0].replace("\\", "/")
-            env["PATH"].insert(0, ninjaDir)
+            # Explicit use external Ninja
+            if self.options.ninja:
+                ninja_binpath = self.deps_cpp_info["ninja_installer"].bin_paths[0].replace("\\", "/")
+                env["PATH"].insert(0, ninja_binpath)
+        # Run build
         with tools.chdir("src"), tools.environment_append(env):
             self.run("python --version")
-            self.run("ninja --version")
             #
             self.run("python configure %s" % " ".join(flags))
-            self.run("ninja -w dupbuild=warn -d keeprsp --verbose -C out/%s" % str(self.settings.build_type))
+            if self.options.ninja:
+                self.run("ninja --version")
+                self.run("ninja -C out/%s" % str(self.settings.build_type))
+            elif self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+                msbuild = MSBuild(self)
+                msbuild.build("node.sln", targets=["Build"], upgrade_project=False, verbosity="normal", use_env=False)
+            else:
+                self.run("make -j %s" % tools.cpu_count())
 
     def package(self):
         # CMake script
@@ -137,16 +141,18 @@ class JScriptConan(ConanFile):
         self.copy("*oda/jscript.h", dst="include/oda", keep_path=False)
         self.copy("*node.h", dst="include", keep_path=False)
         self.copy("*node_version.h", dst="include", keep_path=False)
-        #self.copy("*node_internals.h", dst="include", keep_path=False)
         src_v8_headers = os.path.join(self.build_folder, "src", "deps", "v8", "include")
         self.copy("*.h", src=src_v8_headers, dst="include", keep_path=True)
         # Libraries
         self.copy("*jscript.dll.lib", dst="lib", keep_path=False)
         self.copy("*jscript.dll", dst="bin", keep_path=False)
+        self.copy("*jscriptd.lib", dst="lib", keep_path=False)
         self.copy("*jscriptd.dll.lib", dst="lib", keep_path=False)
         self.copy("*jscriptd.dll", dst="bin", keep_path=False)
+        self.copy("*jscript64.lib", dst="lib", keep_path=False)
         self.copy("*jscript64.dll.lib", dst="lib", keep_path=False)
         self.copy("*jscript64.dll", dst="bin", keep_path=False)
+        self.copy("*jscript64d.lib", dst="lib", keep_path=False)
         self.copy("*jscript64d.dll.lib", dst="lib", keep_path=False)
         self.copy("*jscript64d.dll", dst="bin", keep_path=False)
         if self.settings.os == "Linux":
@@ -180,6 +186,9 @@ class JScriptConan(ConanFile):
                     cmd = windows_signtool.get_sign_command(fpath, digest_algorithm=alg, timestamp=is_timestamp)
                     self.output.info("Sign %s" % fpath)
                     self.run(cmd)
+
+    def package_id(self):
+        self.info.options.ninja = "any"
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
