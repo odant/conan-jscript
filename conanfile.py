4825e80e5ea18c43553c7085b1ee5b2b44ee0b2b
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-12-18 02:26:48 +0300

    Bump next version

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ import os, glob, re
 
 class JScriptConan(ConanFile):
     name = "jscript"
-    version = "14.15.2.1"
+    version = "14.15.2.2"
     license = "Node.js https://raw.githubusercontent.com/nodejs/node/master/LICENSE"
     description = "Odant Jscript"
     url = "https://github.com/odant/conan-jscript"
