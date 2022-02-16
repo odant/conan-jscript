20d4c996a00bdd636e54aa5d0a985ad688f4e033
d0nc1h0t <udincev@yandex.ru>
2022-02-16 12:04:53 +0300

    Add a script for a quick start of debugging

---
+++ prepare_for_debugging.bat
@@ -0,0 +1,6 @@
+git apply .\oda.patch
+git apply .\fix_vs2022_build.patch
+git apply .\use_nodepath_for_esm.patch
+git apply .\experimental.patch
+git apply .\fix_no_optimization_build.patch
+git apply .\fix_vcbuild.patch
