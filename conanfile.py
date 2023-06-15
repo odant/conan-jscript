334816a43497b99b2559e446513dae8e7cb9744c
d0nc1h0t <udincev@yandex.ru>
2023-06-15 13:45:40 +0300

    Version 18.16.0.2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ import os, glob, re
 
 class JScriptConan(ConanFile):
     name = "jscript"
-    version = "18.16.0.1"
+    version = "18.16.0.2"
     license = "Node.js https://raw.githubusercontent.com/nodejs/node/master/LICENSE"
     description = "Odant Jscript"
     url = "https://github.com/odant/conan-jscript"
