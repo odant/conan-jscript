d46c3a91c175d84757f7e7a40d8aba6ecf01ed24
d0nc1h0t <udincev@yandex.ru>
2023-10-11 15:49:15 +0300

    Adopt patches to version 18.18.1

--- conanfile.py
+++ conanfile.py
@@ -47,8 +47,7 @@ class JScriptConan(ConanFile):
         "fix_no_optimization_build.patch",
         "disable_v8_slow_dcheck.patch",
         "disable_gen_node_def.patch",
-        "fix_deps_undici.patch",
-        "libuv_win7support.patch"
+        "fix_deps_undici.patch"
     ]
     no_copy_source = False
     build_policy = "missing"
@@ -83,7 +82,6 @@ class JScriptConan(ConanFile):
         if self.settings.os == "Windows":
             tools.patch(patch_file="fix_vs2022_build.patch")
             tools.patch(patch_file="disable_gen_node_def.patch")
-            tools.patch(patch_file="libuv_win7support.patch")
         if self.settings.build_type == "Debug":
 #            if self.settings.os == "Windows":
 #                tools.patch(patch_file="fix_no_optimization_build.patch")
